0d7a1b88f0c969954a749e8ded0afb46e464703f
MariusB <marius.budreika09@gmail.com>
2026-03-31 14:20:41 +0300

    Exercise 3.2 added extra info route with some additional info

--- FullStackOpenPart3/index.js
+++ FullStackOpenPart3/index.js
@@ -32,6 +32,12 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  response.send(
+    `<div>Phonebook has info for ${persons.length} people</div><h4>${new Date()}</h4>`,
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
