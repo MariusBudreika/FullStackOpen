d6f6a53b8c63a67d7160ee94a8a623e1035a2c82
MariusB <marius.budreika09@gmail.com>
2026-03-31 15:13:11 +0300

    Exercise 3.5 POST new person with Math.random function id.

--- FullStackOpenPart3/index.js
+++ FullStackOpenPart3/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let persons = [
   {
@@ -23,6 +24,12 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+const generateId = () => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((p) => Number(p.id))) : 0;
+  return String(maxId + 1);
+};
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -54,6 +61,26 @@ app.delete("/api/persons/:id", (request, response) => {
 
   persons = persons.filter((person) => person.id !== id);
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
