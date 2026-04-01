3f2bd46b553b0032a7573406d57c8ab6776d539d
MariusB <marius.budreika09@gmail.com>
2026-04-01 13:38:57 +0300

    Configured port information for hosting service

--- FullStackOpenPart3/index.js
+++ FullStackOpenPart3/index.js
@@ -104,9 +104,10 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 /* const express = require("express");
 const app = express();
